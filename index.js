8e47e1be73b0a7921d5ad0f3731ff0e6c59703a0
gaurv123 <91819071+gaurv123@users.noreply.github.com>
2022-01-08 11:59:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-var num1=Math.floor(6*Math.random()+1);
-var num2=Math.floor(6*Math.random()+1);
-var num;
+var num1;
+var num2;
+
+document.querySelector(".img1").addEventListener("click",function(){
+     num1=Math.floor(6*Math.random()+1);
+     num2=Math.floor(6*Math.random()+1);
 if(num1>num2){
-document.querySelector(".verdict").innerHTML="PLAYER1 WON";
-}
-if(num1<num2){
-document.querySelector(".verdict").innerHTML="PLAYER2 WON";
-}
-if(num1===num2){
-document.querySelector(".verdict").innerHTML="IT's a Tie";
-}
-if(num1){
-    imgChange(num1);
-   
-}
-if(num2){
-    imgChange2(num1);
-    
-}
-function imgChange(){
-    document.querySelector(".img1").src="./images/dice"+num1+".png";
-}
-   
-   
-function imgChange2(){
-    document.querySelector(".img2").src="./images/dice"+num2+".png";
-}
+    document.querySelector(".verdict").innerHTML="you win";
+    }
+    if(num1<num2){
+    document.querySelector(".verdict").innerHTML="you lose";
+    }
+    if(num1===num2){
+    document.querySelector(".verdict").innerHTML="IT's a tie";
+    }
+    if(num1){
+        imgChange(num1);
+       
+    }
+    if(num2){
+        imgChange2(num1);
+        
+    }
+    function imgChange(){
+        document.querySelector(".img1").src="./images/dice"+num1+".png";
+    }
+       
+       
+    function imgChange2(){
+        document.querySelector(".img2").src="./images/dice"+num2+".png";
+    }
+
+
+})
